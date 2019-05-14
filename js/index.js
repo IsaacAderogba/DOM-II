0d8ac06f5e25e97611f050138523f8a11728622c
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-14 12:36:04 +0100

    Add button mouseover effect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// 1. mouseover button effect
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(function(button) {
+    button.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'black');
+});
+
+// buttons.addEventListener('mouseover', function(event){
+//     event.target.style.backgroundColor = 'ffeacc';
+// });
+
+// 2. keydown to implement dark mode
+
+// 3. wheel to zoom in bus picture
+
+// 4. Drag either let's go image or adventure waits image over eachother
+
+// 5. Congratulate user for successfully loading the page
+
+// 6. focus on a heading paints a beach background
+
+// 7. As window is resized, invert sizes
+
+// 8. Scroll progressively changes the header background colour
+
+// 9. Bold and italicise any text that has been selected
+
+// 10. Increase size of any 3 images if double clicked
