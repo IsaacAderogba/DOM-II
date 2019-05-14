80b077ac2425516efe3f1d2e41479a8ef51ec1a0
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-05-14 13:00:16 +0100

    Add keydown dark mode effect

--- js/index.js
+++ js/index.js
@@ -3,15 +3,25 @@
 // 1. mouseover button effect
 const buttons = document.querySelectorAll('.btn');
 
-buttons.forEach(function(button) {
+buttons.forEach((button) => {
     button.addEventListener('mouseover', (event) => event.target.style.backgroundColor = 'black');
 });
-
-// buttons.addEventListener('mouseover', function(event){
-//     event.target.style.backgroundColor = 'ffeacc';
-// });
-
 // 2. keydown to implement dark mode
+const body = document.body;
+const navLinks = document.querySelectorAll('.main-navigation .nav-link')
+
+document.addEventListener('keydown', () => {
+    body.classList.toggle('darklightmode');
+    if(body.classList.contains('darklightmode')) {
+        body.setAttribute('style', 'color: white; background: #2d2d2d;');
+        body.children[0].setAttribute('style', 'color: white; background: #2d2d2d;');
+        navLinks.forEach(navLink => navLink.setAttribute('style', 'color: white'));
+    } else {
+        body.removeAttribute('style');
+        body.children[0].removeAttribute('style');
+        navLinks.forEach(navLink => navLink.removeAttribute('style'));
+    }
+});
 
 // 3. wheel to zoom in bus picture
 
